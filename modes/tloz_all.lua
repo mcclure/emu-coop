9d82e6172b41efc4e5d31b6270957723d79e5e77
megmacattack <megmacatttack@gmail.com>
2018-02-25 19:22:34 -0700

    z1: rename sync everything to sync most of the things
    Also change the order so that the most commonly used option is first.

--- modes/tloz_all.lua
+++ modes/tloz_all.lua
@@ -17,7 +17,7 @@ local base_spec = require('modes.tloz_progress')
 local spec = {
 	guid = "af791fc5-eeb2-49dc-985c-2ecba72157d9",
 	format = "1.1",
-	name = "The Legend of Zelda (sync everything)",
+	name = "The Legend of Zelda (sync most of the things)",
 	match = {"stringtest", addr=0xffeb, value="ZELDA"},
 
 	sync = {},
